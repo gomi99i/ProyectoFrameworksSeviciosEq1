30e2703db2682d1aa80547e329df3d253615365b
Invitado <Invitado@DESKTOP-GV7TU13>
2021-04-28 12:36:53 -0500

    Prueba para servicios

---
+++ src/main/java/cursoDAgil/service/cliente/ClienteServiceImpl.java
@@ -0,0 +1,22 @@
+package cursoDAgil.service.cliente;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cursoDAgil.bd.domain.Cliente;
+import cursoDAgil.dao.cliente.ClienteDao;
+
+@Named
+public class ClienteServiceImpl implements ClienteService, Serializable {
+	private static final long serialVersionUID = -3636050252009531409L;
+	@Inject
+	ClienteDao clienteDao;
+	
+	@Override
+	public List<Cliente> listarTodosClientes() {
+		return clienteDao.listarTodosClientes();
+	}
+}
---
+++ src/test/java/cursoDAgil/service/cliente/ClienteServiceImplTest.java
@@ -0,0 +1,33 @@
+package cursoDAgil.service.cliente;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cursoDAgil.bd.domain.Cliente;
+import cursoDAgil.service.cliente.ClienteService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
+public class ClienteServiceImplTest {
+	
+	@Inject
+	ClienteService clienteService;
+	
+	@Test
+	public void pruebaConsultarTodo(){
+		try{
+			List<Cliente> lista = clienteService.listarTodosClientes();
+			assertEquals(lista.size(),3);
+		} catch(Exception e){
+			System.out.println("Error: " + e);
+		}
+	}
+}
